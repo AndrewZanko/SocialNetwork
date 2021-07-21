065f705d360d1063deacdca7b6f9744dc5c7b220
Andrew <sem.andrej9@gmail.com>
2021-07-21 17:26:33 +0300

    Added grid to Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,36 @@ import d from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={d.dialogs}>
+            <div className={d.dialogsItems}>
+                <div className={d.dialogs + ' ' + d.active}>
+                    Andrew
+                </div>
+                <div className={d.dialogs}>
+                    Oleg
+                </div>
+                <div className={d.dialogs}>
+                    Pasha
+                </div>
+                <div className={d.dialogs}>
+                    Denis
+                </div>
+                <div className={d.dialogs}>
+                    Katya
+                </div>
+            </div>
+            <div className={d.messages}>
+                <div className={d.message}>
+                    Hi
+                </div>
+                <div className={d.message}>
+                    Going well!
+                </div>
+                <div className={d.message}>
+                    Any plans for tonight?
+                </div>
+            </div>
+        </div>
     );
 };
 
