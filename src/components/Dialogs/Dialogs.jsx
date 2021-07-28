c7158fdb3c01579bea7c22f24e54b2b36271a8d8
Andrew <sem.andrej9@gmail.com>
2021-07-28 13:02:42 +0300

    Added state

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,8 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let dialogsElements = props.dialogs.map((di, index) => { return <DialogItem name={di.name} id={di.id} key={index}/>});
-    let messagesElements = props.messages.map((m, index) => {return <Message message={m.message} key={index}/>})
+    let dialogsElements = props.state.dialogs.map((di, index) => { return <DialogItem name={di.name} id={di.id} key={index}/>});
+    let messagesElements = props.state.messages.map((m, index) => {return <Message message={m.message} key={index}/>})
 
     return (
         <div className={d.dialogs}>
---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    profilePage : {
+        posts : [
+            {id: 1, message: "what's up, dudes", likesCount: 15},
+            {id: 2, message: "going well XD", likesCount: 20},
+        ],
+    },
+    dialogsPage : {
+        dialogs : [
+            {id: 1, name: "Andrew"},
+            {id: 2, name: "Oleg"},
+            {id: 3, name: "Pasha"},
+            {id: 4, name: "Denis"},
+            {id: 5, name: "Katya"},
+        ],
+        messages : [
+            {id: 1, message: "Hi!"},
+            {id: 2, message: "Going well!"},
+            {id: 3, message: "Any plans for tonight?"},
+        ],
+    }    
+};
+
+export default state;
