ab39b9defb356293a1f5a5cb97ba8b2895c0cd42
Andrew <sem.andrej9@gmail.com>
2021-08-02 17:21:51 +0300

    Improved addPost passing

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { globalRerender } from "../render";
+
 let state = {
     profilePage : {
         posts : [
@@ -38,9 +40,9 @@ let state = {
 };
 
 export let addPost = (postMessage) => {
-    debugger;
     let newPost = {id: 5, message: postMessage, likesCount: 0}; 
     state.profilePage.posts.push(newPost);
+    globalRerender(state);
 }
 
 export default state;
