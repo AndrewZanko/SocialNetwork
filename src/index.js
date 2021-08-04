1554451bcbe5f542828b9b77acd1b445b081b1fd
Andrew <sem.andrej9@gmail.com>
2021-08-04 19:07:34 +0300

    Added store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addMessage, addPost, subscribe, updateMessageText, updatePostText} from './redux/state';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
+import store from './redux/state';
 
 export let globalRerender = (state) => {
     ReactDOM.render(
       <React.StrictMode>
-        <App state={state} addPost={addPost} updatePostText={updatePostText}
-        addMessage={addMessage} updateMessageText={updateMessageText}/>
+        <App state={state} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)}
+        addMessage={store.addMessage.bind(store)} updateMessageText={store.updateMessageText.bind(store)}/>
       </React.StrictMode>,
       document.getElementById('root')
     );
   };
 
-globalRerender(state);
+globalRerender(store.getState());
 
-subscribe(globalRerender);
+store.subscribe(globalRerender);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,73 +1,64 @@
-let globalRerender = () => {
-}
-
-let state = {
-    profilePage : {
-        posts : [
-            {id: 1, message: "what's up, dudes", likesCount: 15},
-            {id: 2, message: "going well XD", likesCount: 20},
-        ],
-        postText : "Mock-text",
+let store = {
+    _state: {
+        profilePage : {
+            posts : [
+                {id: 1, message: "what's up, dudes", likesCount: 15},
+                {id: 2, message: "going well XD", likesCount: 20},
+            ],
+            postText : "New post",
+        },
+        dialogsPage : {
+            dialogs : [
+                {id: 1, name: "Misha", ava: "https://pixelbox.ru/wp-content/uploads/2018/02/john_wick_steam_avatars.jpg"},
+                {id: 2, name: "Oleg", ava: "https://i.pinimg.com/736x/c2/74/aa/c274aa153d376a2365b66af27521d203.jpg"},
+                {id: 3, name: "Lena", ava: "https://i.pinimg.com/564x/2c/60/cb/2c60cb34a209daa60ee0b6c53cd35688.jpg"},
+                {id: 4, name: "Denis", ava: "https://meragor.com/files/styles//ava_800_800_wm/vouwzf8qak4.jpg"},
+                {id: 5, name: "Katya", ava: "https://game-roblox.ru/wp-content/uploads/2020/07/unnamed-2.jpg"},
+            ],
+            messages : [
+                {id: 1, message: "Hi!"},
+                {id: 2, message: "Going well!"},
+                {id: 3, message: "Any plans for tonight?"},
+            ],
+            messageText : "New message",
+        }
     },
-    dialogsPage : {
-        dialogs : [
-            {id: 1, name: "Misha", ava: "https://pixelbox.ru/wp-content/uploads/2018/02/john_wick_steam_avatars.jpg"},
-            {id: 2, name: "Oleg", ava: "https://i.pinimg.com/736x/c2/74/aa/c274aa153d376a2365b66af27521d203.jpg"},
-            {id: 3, name: "Lena", ava: "https://i.pinimg.com/564x/2c/60/cb/2c60cb34a209daa60ee0b6c53cd35688.jpg"},
-            {id: 4, name: "Denis", ava: "https://meragor.com/files/styles//ava_800_800_wm/vouwzf8qak4.jpg"},
-            {id: 5, name: "Katya", ava: "https://game-roblox.ru/wp-content/uploads/2020/07/unnamed-2.jpg"},
-        ],
-        messages : [
-            {id: 1, message: "Hi!"},
-            {id: 2, message: "Going well!"},
-            {id: 3, message: "Any plans for tonight?"},
-        ],
-        messageText : "Mock-text",
-        // messages : [
-        //     {id: 1, name: "Misha", messages:[{id: 1, author: "Andrew", message: "Hi!"},
-        //                                      {id: 2, author: "Misha", message: "Hey, how's it going?"},
-        //                                      {id: 3, author: "Andrew", message: "Pretty well, I've got so much to tell you!"}]},                                            
-        //     {id: 2, name: "Oleg", messages: [{id: 1, author: "Oleg", message: "Are you here?"},
-        //                                      {id: 2, author: "Andrew", message: "Meet you in 10 minutes"},
-        //                                      {id: 3, author: "Oleg", message: "Ok"}]},
-        //     {id: 3, name: "Lena", messages: [{id: 1, author: "Lena", message: "Have you read '1984'?"},
-        //                                      {id: 2, author: "Andrew", message: "Yeah, btw not long ago"},
-        //                                      {id: 3, author: "Lena", message: "Same XD It reminds me current events"}]},
-        //     {id: 4, name: "Denis", messages:[{id: 1, author: "Andrew", message: "I found it"},
-        //                                      {id: 2, author: "Denis", message: "Nice"},
-        //                                      {id: 3, author: "Denis", message: "Don't forget to bring it next time"}]},
-        //     {id: 5, name: "Katya", messages: [{id: 1, author: "Katya", message: "That was hilarios"},
-        //                                      {id: 2, author: "Andrew", message: "Yeah I've laughed so much"},]},
-        // ]
-    }    
-};
 
-export const addPost = () => {
-    let newPost = {id: 5, message: state.profilePage.postText, likesCount: 0}; 
-    state.profilePage.posts.push(newPost);
-    state.profilePage.postText = '';
-    globalRerender(state);
+    _callSubscriber() {
+        
+    },
+
+    getState() {
+        return this._state;
+    },
+
+    addPost() {
+        let newPost = {id: 5, message: this._state.profilePage.postText, likesCount: 0}; 
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.postText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updatePostText(newText) {
+        this._state.profilePage.postText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    addMessage() {
+        let newMessage = {id: 4, message: this._state.dialogsPage.messageText};
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.messageText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateMessageText(newText) {
+        this._state.dialogsPage.messageText = newText;
+        this._callSubscriber(this._state); 
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer; 
+    }
 }
 
-export const updatePostText = (newText) => {
-    state.profilePage.postText = newText;
-    globalRerender(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {id: 4, message: state.dialogsPage.messageText};
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.messageText = '';
-    globalRerender(state);
-}
-
-export const updateMessageText = (newText) => {
-    state.dialogsPage.messageText = newText;
-    globalRerender(state);
-}
-
-export let subscribe = (observer) => {
-    globalRerender = observer;
-} 
-
-export default state;
+export default store;
