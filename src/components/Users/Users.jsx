5460742ea49ca9ef65802c3e62ba0fc4cada0ba3
Andrew <sem.andrej9@gmail.com>
2021-08-13 12:41:56 +0300

    Added implementation of follow/unfollow buttons in user page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import styles from './Users.module.css';
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        props.setUsers([ {id: 1, fullName: 'Slava', isFollowed: true, status: 'Street racer', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://uznayvse.ru/images/celebs/2018/1/vyacheslav-chepurchenko_big.jpg'},
+        {id: 2, fullName: 'Marina', isFollowed: false, status: 'Professional photographer', location: {city: 'Moscow', country: 'Russia'}, photoUrl: 'https://uznayvse.ru/images/celebs/2020/2/marina-orlova_big.jpg'},
+        {id: 3, fullName: 'Gleb', isFollowed: true, status: 'Insert status here', location: {city: 'Brest', country: 'Belarus'}, photoUrl: 'https://fcminsk.by/wp-content/uploads/2001/05/gleb-gurban.png'},
+    ]);
+    }
     return (
         <div>
             {
@@ -8,10 +15,12 @@ const Users = (props) => {
                     return (<div key = {index}>
                         <div>
                             <div>
-                                <img src={u.photoUrl} alt='profile'></img>
+                                <img src={u.photoUrl} alt='profile' className={styles.userPhoto}></img>
                             </div>
                             <div>
-                                <button>Follow</button>
+                                {u.isFollowed ? 
+                                <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button> :
+                                <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
                             </div>
                         </div>
                         <div>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,11 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let inintialState = {
-    users : [
-        {id: 1, fullName: 'Slava', isFollowed: true, status: 'Street racer', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://uznayvse.ru/images/celebs/2018/1/vyacheslav-chepurchenko_big.jpg'},
-        {id: 2, fullName: 'Marina', isFollowed: false, status: 'Professional photographer', location: {city: 'Moscow', country: 'Russia'}, photoUrl: 'https://uznayvse.ru/images/celebs/2020/2/marina-orlova_big.jpg'},
-        {id: 3, fullName: 'Gleb', isFollowed: true, status: 'Insert status here', location: {city: 'Brest', country: 'Belarus'}, photoUrl: 'https://fcminsk.by/wp-content/uploads/2001/05/gleb-gurban.png'},
-    ],
+    users : [],
 };
 
 const usersReducer = (state = inintialState, action) => {
