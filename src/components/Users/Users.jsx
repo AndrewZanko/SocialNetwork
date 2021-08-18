a0a420ba047ee5c72a2ea8ecced3113967cb86f4
Andrew <sem.andrej9@gmail.com>
2021-08-18 13:29:45 +0300

    Added second container component for Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,71 +1,51 @@
 import React from 'react';
 import styles from './Users.module.css';
-import * as axios from 'axios';
 import userPhoto from './../../assets/images/user.png'
 
-class Users extends React.Component {
+const Users = (props) => {
 
-    componentDidMount() {
-        // props.setUsers([ {id: 1, fullName: 'Slava', isFollowed: true, status: 'Street racer', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://uznayvse.ru/images/celebs/2018/1/vyacheslav-chepurchenko_big.jpg'},
-        // {id: 2, fullName: 'Marina', isFollowed: false, status: 'Professional photographer', location: {city: 'Moscow', country: 'Russia'}, photoUrl: 'https://uznayvse.ru/images/celebs/2020/2/marina-orlova_big.jpg'},
-        // {id: 3, fullName: 'Gleb', isFollowed: true, status: 'Insert status here', location: {city: 'Brest', country: 'Belarus'}, photoUrl: 'https://fcminsk.by/wp-content/uploads/2001/05/gleb-gurban.png'},
-        // ]);
-    
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount)});
-    }
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for(let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    };
 
-    onPageChainged(pageNumber) {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
-            this.props.setUsers(response.data.items)});
-    }
-
-    render () {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for(let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    {pages.map(p => {
-                        return (<span className={this.props.currentPage === p && styles.selectedPage}
-                            onClick={(e)=>{this.onPageChainged(p)}}>{p}</span>);
+                {pages.map(p => {
+                    return (<span className={props.currentPage === p && styles.selectedPage}
+                        onClick={(e)=>{props.onPageChainged(p)}}>{p}</span>);
                     })}
-                </div>
-                {
-                    this.props.users.map( (u, index) => {
-                        return (<div key = {index}>
-                            <div>
-                                <div>
-                                    <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='profile' className={styles.userPhoto}></img>
-                                </div>
-                                <div>
-                                    {u.isFollowed ? 
-                                    <button onClick={()=>{this.props.unfollow(u.id)}}>Unfollow</button> :
-                                    <button onClick={()=>{this.props.follow(u.id)}}>Follow</button>}
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <div>{u.name}</div>
-                                    <div>{u.status}</div>
-                                </div>
-                                <div>
-                                    <div>{"u.location.city"}</div>
-                                    <div>{"u.location.country"}</div>
-                                </div>
-                            </div>
-                        </div>);
-                    })
-                }
             </div>
-        )
-    }
-};
+            {
+                props.users.map( (u, index) => {
+                    return (<div key = {index}>
+                        <div>
+                            <div>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='profile' className={styles.userPhoto}></img>
+                            </div>
+                            <div>
+                                {u.isFollowed ? 
+                                    <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button> :
+                                    <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </div>
+                            <div>
+                                <div>{"u.location.city"}</div>
+                                <div>{"u.location.country"}</div>
+                            </div>
+                        </div>
+                    </div>);
+                })
+            }
+        </div>
+    )
+}
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,42 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from '../../redux/usersReducer';
+import * as axios from 'axios';
 import Users from './Users';
+
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        // props.setUsers([ {id: 1, fullName: 'Slava', isFollowed: true, status: 'Street racer', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://uznayvse.ru/images/celebs/2018/1/vyacheslav-chepurchenko_big.jpg'},
+        // {id: 2, fullName: 'Marina', isFollowed: false, status: 'Professional photographer', location: {city: 'Moscow', country: 'Russia'}, photoUrl: 'https://uznayvse.ru/images/celebs/2020/2/marina-orlova_big.jpg'},
+        // {id: 3, fullName: 'Gleb', isFollowed: true, status: 'Insert status here', location: {city: 'Brest', country: 'Belarus'}, photoUrl: 'https://fcminsk.by/wp-content/uploads/2001/05/gleb-gurban.png'},
+        // ]);
+    
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount)});
+    }
+
+    onPageChainged(pageNumber) {
+        debugger;
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
+            this.props.setUsers(response.data.items)});
+    }
+
+    render () {
+        return (
+            <Users totalUsersCount = {this.props.totalUsersCount} 
+                pageSize = {this.props.pageSize}
+                currentPage = {this.props.currentPage}
+                onPageChainged = {this.onPageChainged.bind(this)}
+                users = {this.props.users}
+                follow = {this.props.follow}
+                unfollow = {this.props.unfollow}/>
+        );
+    }
+};
 
 const mapStateToProps = (state) => {
     return {
@@ -31,6 +67,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-    
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
