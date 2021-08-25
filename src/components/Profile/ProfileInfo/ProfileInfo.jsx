baf79cd1e75ff312b8f8213ec5a37a484603d824
Andrew <sem.andrej9@gmail.com>
2021-08-25 13:38:45 +0300

    Implemented local state to profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import pi from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/images/user.png';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,13 +10,14 @@ const ProfileInfo = (props) => {
     };
     return (
         <div>
-            <div>
+            {/* <div>
                 <img className={pi.img} src="https://www.potidea-palace.com/wp-content/uploads/elementor/thumbs/A-beach-to-behold-Halkidiki%E2%80%99s-most-unique-beaches-ol5gkj7ff58eyfz0tipioyfe2zgpuroidmogct6g6o.jpg" alt="beach" />
-            </div>
+            </div> */}
             <div className={pi.avaDesrBlock}>
                 <h2>{props.profile.fullName}</h2>
                 <img src={props.profile.photos.large ? props.profile.photos.large : userPhoto} alt='profile logo'></img>
                 <p>About me: {props.profile.aboutMe}</p>
+                <ProfileStatus status='status'/>
             </div>
         </div>
     );
